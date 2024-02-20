39d597c79c7af98c211322b373bcb122b9ab3b14
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-20 11:52:42 -0700

    fixed timer according to feedback, within user commandline

--- timer1.js
+++ timer1.js
@@ -1,29 +1,20 @@
-/*
-Challenge
-----------------
-Instruction
-----------------
-Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
 
-/ https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
-
-Will be using setTimeout to schedule the timer use cb to produce windows beep
-*/
-process.stdout.write('\x07');
-
-//check for edge cases, time is not valid number, less than or equal to 0, and ignoring negative/non numbers
-function setTimer(time) { 
-  if (typeof time !== 'number' || time <= 0) return;
-    setTimeout(function() {
-      process.stdout.write('\x07'); 
-    }, time * 1000);
-};
+//check for edge cases 
+function setTimers(times) {
+  times.forEach(time => {
+     //time is valid number,greater than 0
+    if (typeof time === 'number' && time > 0) {
+      // scheduled beep
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, time * 1000);
+    }
+  });
+}
 
 
+// using command line arugments (excludes first two lines)
+const args = process.argv.slice(2).map(Number);
 
-// Schedule alarms with predefined time values (in seconds)
-setTimer(3); // Beep after 3 seconds
-setTimer(5); // Beep after 5 seconds
-setTimer(9); // Beep after 9 seconds
-setTimer(10); // Beep after 10 seconds
-setTimer(15); // Beep after 15 seconds
+// calling setTimers() with user input
+setTimers(args);
